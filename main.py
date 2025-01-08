2ab37f120e5482acbf02ac038dd386a15ddfa6f1
Long17369 <long17368@foxmail.com>
2025-01-08 22:02:09 +0800

    重构主函数，封装游戏初始化和主循环逻辑

--- main.py
+++ main.py
@@ -1,31 +1,37 @@
 import pygame
 from snd import *
 
-# Initialize the game
-pygame.init()
-pygame.display.set_caption(CONST.WINDOWS_TITLE)
-screen = pygame.display.set_mode(CONST.WINDOWS_SIZE)
-clock = pygame.time.Clock()
 
-# Game loop
-game = Game(is_print=True)
+def main():
+    # Initialize the game
+    pygame.init()
+    pygame.display.set_caption(CONST.WINDOWS_TITLE)
+    screen = pygame.display.set_mode(CONST.WINDOWS_SIZE)
+    clock = pygame.time.Clock()
 
-while(True):
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit(0)
-        if event.type == pygame.KEYDOWN:
-            game.keydown(event)
-        if event.type == pygame.KEYUP:
-            game.keyup(event)
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            game.mouse_down(event)
-        if event.type == pygame.MOUSEBUTTONUP:
-            game.mouse_up(event)
-    if game.run == False:
-        game = Game(is_print=True)
-    game.update()
-    pygame.display.flip()
-    clock.tick(60)
+    # Game loop
+    game = Game()
 
+    while(True):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                del game
+                pygame.quit()
+                return
+            if event.type == pygame.KEYDOWN:
+                game.keydown(event)
+            if event.type == pygame.KEYUP:
+                game.keyup(event)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                game.mouse_down(event)
+            if event.type == pygame.MOUSEBUTTONUP:
+                game.mouse_up(event)
+        if game.run == False:
+            del game
+            game = Game()
+        game.update()
+        pygame.display.flip()
+        clock.tick(60)
+
+if __name__ == "__main__":
+    main()
